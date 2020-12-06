4482f406bbbcb86860b89434e472d313d4f94564
Marina Monsivais <minamonsi@me.com>
2020-12-05 21:30:45 -0600

    Added 3 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+//  Your code goes here
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener("click", (e) => {
+  alert("Let's plan a trip!");
+});
+
+window.addEventListener("load", (e) => {
+  console.log(`${event.type} happened`);
+});
+
+const intro = document.querySelector(".intro");
+console.log(intro);
+intro.addEventListener("mouseenter", (e) => {
+  intro.style.backgroundColor = "pink";
+});
