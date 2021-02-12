97248f64d5dc0568031ad1d637cb365e10eb00fd
Marina Monsivais <minamonsi@me.com>
2021-02-12 13:52:04 -0600

    Created 2 more eventListners

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
 //  Your code goes here
+//1. CLICK EventListner
 const logo = document.querySelector(".logo-heading");
 logo.addEventListener("click", (e) => {
   alert("Let's plan a trip!");
 });
 
+//2. LOAD EventListner
 window.addEventListener("load", (e) => {
   console.log(`${event.type} happened`);
 });
 
+//3. MOUSEENTER EventListner
 const introImg = document.querySelector(".intro img");
-console.log(introImg);
-
+//console.log(introImg);
 introImg.addEventListener("mouseenter", () => {
    introImg.style.transform = "scale(1.2)";
    introImg.style.transition = "all 0.3s";
  });
 
-
+//4. FOCUS EventListner
 const navLinks = document.querySelectorAll(".nav-link");
 //console.log(navLinks);
-
 navLinks.forEach(links => {
   links.addEventListener('focus',(event) => {
     event.target.style.color = "red"
@@ -27,3 +28,25 @@ navLinks.forEach(links => {
   })
 });
 
+//5. RESIZE EventListener
+const imgContent = document.querySelector(".img-content img");
+//console.log(imgContent);
+ window.addEventListener('resize', () => {
+   imgContent.src = "https://www.fodors.com/wp-content/uploads/2018/09/4_MachuPicchu101_WhereCanIGetBestPhoto_dreamstime_xxl_55205752_1-724x483.jpg"
+ })
+
+ //6. MOUSEOVER EventListner
+ const destination = document.querySelector(".content-destination")
+ //console.log(destination);
+ destination.addEventListener("mouseover", (event) => {
+   event.stopPropagation();
+   event.target.style.backgroundColor = "aqua";
+   event.target.style.color = "#7a38f5"
+   console.log("End destination content.")
+ })
+
+ //7. WHEEL EventListner
+ const advAwaits = document.querySelector(".text-content");
+ console.log(advAwaits);
+
+ 
