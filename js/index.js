07d0a200453333c8bc05ac5ed13332a26d5e58da
Marina Monsivais <minamonsi@me.com>
2021-02-12 00:15:51 -0600

    modified mouseenter event

--- js/index.js
+++ js/index.js
@@ -8,8 +8,19 @@ window.addEventListener("load", (e) => {
   console.log(`${event.type} happened`);
 });
 
-const intro = document.querySelector(".intro");
-console.log(intro);
-intro.addEventListener("mouseenter", (e) => {
-  intro.style.backgroundColor = "pink";
+const introImg = document.querySelector(".intro img");
+console.log(introImg);
+
+introImg.addEventListener("mouseenter", () => {
+   introImg.style.transform = "scale(1.2)";
+   introImg.style.transition = "all 0.3s";
+ });
+
+
+const navLinks = document.querySelector(".nav-link");
+//console.log(navLinks);
+
+navLinks.addEventListener('focus',(event) => {
+  event.target.style.background = 'yellow';
+  event.target.style.color = "red";
 });
